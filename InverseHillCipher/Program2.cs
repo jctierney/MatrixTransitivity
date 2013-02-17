413af233c07d8af96e55c1aad061645f400e043d
Jason Tierney <jason.tierney@live.com>
2013-02-16 23:08:21 -0500

    Removed comments

---
+++ InverseHillCipher/Program2.cs
@@ -0,0 +1,48 @@
+﻿/******************************************************************************
+ * File: Program2.cs
+ * Description: The same as Program.cs, but formatted using Java style to
+ *              slim the line count down.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InverseHillCipher {
+    public class Program2 {
+        /// <summary>
+        /// Main entry point to the program.
+        /// </summary>
+        static void Main(string[] args){
+            int num;
+            Console.WriteLine("Welcome!");
+            Console.Write("Please enter the number: ");
+            string numStr = Console.ReadLine();
+            if (!Int32.TryParse(numStr, out num))
+                Console.WriteLine("Invalid number...");
+            for (int j = 0; j < num; j++) {
+                for (int k = 0; k < 100; k++) {
+                    double temp = 0;
+                    if (j > 0) {
+                        temp = ((double)num * (double)k + (double)1) / (double)j;
+                        if (IsInteger(temp) && temp < num)
+                            Console.WriteLine("{0}: {1}", j, temp);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Determines if the given number is an integer.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        private static bool IsInteger(double num) {
+            if (num == (int)num)
+                return true;
+            return false;
+        }
+    }
+}
--- MatrixTransitive/MatrixTransitivity.cs
+++ MatrixTransitive/MatrixTransitivity.cs
@@ -29,7 +29,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-// Namespace.
 namespace MatrixTransitive
 {
     public class MatrixTransitivity
@@ -42,7 +41,8 @@ namespace MatrixTransitive
             string fileName;
             string tryagain = "y";
             Console.WriteLine("Welcome!");
-            while (tryagain == "y" || tryagain == "Y")
+            
+            while (tryagain.ToUpper().Equals("Y"))
             {
                 Console.WriteLine("Please enter a file name: ");
                 fileName = Console.ReadLine();
@@ -58,40 +58,25 @@ namespace MatrixTransitive
         /// <param name="path"></param>
         private static void ParseFile(string path)
         {           
-            // Reads each line in the given text file and stores it in a string[] array.
             string[] lines = File.ReadAllLines(path);
             int[,] matrix = new int[100, 100];
             int size = 0;
             int place = 0;
-
-            // Iterate through each line.
+            
             for (int i = 0; i < lines.Length; i++)
             {
-                // Parse the line by commas.
-                string[] places = lines[i].Split(',');
-                
-                // Found the end of an array -- we should now check the array.
+                string[] places = lines[i].Split(',');               
                 if (lines[i].Equals("E"))
                 {
-                    PrintMatrix(matrix, size);
-                    
-                    // Reset our vars for the next round.
+                    PrintMatrix(matrix, size);                   
                     matrix = new int[100, 100];
                     place = -1;
                     size = -1;
                 }
                 
-                // Iterate through the number of columns that have a 1.
-                // This is determined when we used the Split function above
-                // to split the line by commas.
-                // NOTE: C# automatically assigns a 0 to an array when we instantiate
-                // a new array, so there is no need to assign 0s, only 1s.
                 for (int j = 0; j < places.Length; j++)
                 {
-                    // Used to determine the column location.
-                    int index = 0;
-                    
-                    // Parse the value in the line and place it in our index.                    
+                    int index = 0;                   
                     if (Int32.TryParse(places[j], out index))
                     {
                         matrix[place, index] = 1;
@@ -110,6 +95,7 @@ namespace MatrixTransitive
         private static void PrintMatrix(int[,] matrix, int size)
         {
             Console.Write("[");
+            
             for (int i = 0; i < size; i++)
             {
                 Console.Write("[");
@@ -118,9 +104,6 @@ namespace MatrixTransitive
                     Console.Write(matrix[i, j].ToString() + " ");
                 }
 
-                // Simply determines if we should add a new line after the ].
-                // If we are at the end of the list, we don't want to add 
-                // a new line.
                 if (i == (size - 1))
                 {
                     Console.Write("]");
@@ -134,7 +117,6 @@ namespace MatrixTransitive
             Console.WriteLine("]");
             string error = string.Empty;
 
-            // Check if our matrix is transitive.
             if (IsTransitive(matrix, size, out error))
             {
                 Console.WriteLine("Matrix is transitive...");
@@ -154,23 +136,19 @@ namespace MatrixTransitive
         /// <returns>True if the matrix is transitive. False if it isn't.</returns>
         private static bool IsTransitive(int[,] matrix, int size, out string error)
         {
-            // Iterate through the matrix.
             for (int i = 0; i < size; i++)
             {
                 for (int j = 0; j < size; j++)
                 {
                     if (matrix[i, j] == 1)
-                    {
-                        // Another iteration -- this compares the matrix with similar pairs.             
+                    {          
                         for (int k = 0; k < size; k++)
                         {
-                            // Checks matrix for transitivity.
-                            // If this is true, the matrix is NOT transitive.
                             if (matrix[j, k] == 1 && matrix[i, k] == 0)
                             {
-                                // Assign our error, return false.
-                                error = "Matrix fails because " + i + ", " + j + " is related to " + 
-                                    j + ", " + k + " but not to " + i + ", " + k + ".";
+                                error = "Matrix fails because " + i + ", " + 
+                                    j + " is related to " + j + ", " + k + 
+                                    " but not to " + i + ", " + k + ".";
                                 return false;
                             }
                         }
@@ -178,7 +156,6 @@ namespace MatrixTransitive
                 }
             }
 
-            // Assign error to empty and return true.
             error = string.Empty;
             return true;
         }
